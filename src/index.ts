ce613b49bd85756a3606fa144b01c196eee50e7e
Bobesz <bob@bobnet.network>
2020-09-30 19:25:31 +0200

    fix models

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+//model
+
+//administration
+export { Option } from './model/administration/Option';
+
+//content
+export { Article, ArticlesMessage, ArticleMessage } from './model/content/Article';
+export { Category, CategoriesMessage, CategoryMessage } from './model/content/Category';
+export { Page } from './model/content/Page';
+export { Tag } from './model/content/Tag';
+
+//user
+export { User } from './model/user/User';
+export { Role } from './model/user/Role';
